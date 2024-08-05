ec8fc1939804df2e17fbb6f8e54d0e6289646748
SainexDo <ventizgene@gmail.com>
2024-08-05 13:31:24 -0600

    El jugador ya puede generar una secuencia de colores.

--- app/game.py
+++ app/game.py
@@ -1,12 +1,9 @@
 from colorama import Fore
 from time import sleep
-import keyboard
 import random
 
 class Game:
-    #Juego, dividido en sus respectivas clases y métodos.
     def __init__(self, rojo, azul, verde, amarillo, reset):
-        #Declaramos variables privadas.
         self.rojo = rojo
         self.azul = azul
         self.verde = verde
@@ -21,28 +18,70 @@ class Game:
             if respuesta not in ('adivinar', 'crear'):
                 print("Agrega una respuesta correcta.")
             else:
+                if respuesta == 'adivinar':
+                    self.creaComputadora()
+                elif respuesta == 'crear':
+                    self.creaJugador()
                 break
 
     def creaJugador(self):
-        print("ga")
+        posiblesOpciones = [self.rojo, self.azul, self.amarillo, self.verde]
+        while True:
+            opcion = input("Elige los colores de tu secuencia (r/b/y/g): ").strip().lower()
+            if len(self.secuencia) < 4:
+                match opcion:
+                    case "r":
+                        self.secuencia.append(posiblesOpciones[0])
+                    case "b":
+                        self.secuencia.append(posiblesOpciones[1])
+                    case "y":
+                        self.secuencia.append(posiblesOpciones[2])
+                    case "g":
+                        self.secuencia.append(posiblesOpciones[3])
+                    case _:
+                        print("Introduzca una respuesta correcta.")
+                print("|", "".join(self.secuencia) + self.reset, "|")
+                if len(self.secuencia) == 4:
+                    confirm = input(f"¿Confirmar secuencia? (S/N): ").strip().lower()
+                    if confirm == "s":
+                        self.eleccionAzar()
+                        break
+                    else: 
+                        self.secuencia = []
+        return self.secuencia
+
 
     def creaComputadora(self):
+        posiblesOpciones = [self.rojo, self.azul, self.amarillo, self.verde]
         while True:
-            posiblesOpciones = (self.rojo, self.azul, self.amarillo, self.verde)
-            self.secuencia.insert(1, random.choice(posiblesOpciones) + self.reset)
-            if len(self.secuencia) == 4:
-                print(''.join(self.secuencia) + self.reset)
-                break
+            self.secuencia = [random.choice(posiblesOpciones) for _ in range(4)]
+            self.eleccionJugador()
+            break
+        print("".join(self.secuencia))
 
-        intentos = []
-        # while True:
-        #     print("Manejo")
-        #     break
+    def eleccionAzar(self):
+        posiblesOpciones = [self.rojo, self.azul, self.amarillo, self.verde]
+        intentos = 0
+        while True:
+            if intentos == 12:
+                print(f"La computadora no ha podido adivinar. ¡Has ganado!")
+                break
+            else:
+                sleep(1)
+                intentos += 1
+                print(f"Intento: {intentos}")
+                eleccionComputadora = [random.choice(posiblesOpciones) for _ in range(4)]
+                print('La elección de la computadora es: ', ''.join(eleccionComputadora) + self.reset)
+                if eleccionComputadora == self.secuencia:
+                    print("¡La computadora ha ganado!")
+                    print(''.join(eleccionComputadora) + self.reset)
+                    break
+
+
 
 def main():
     Juego = Game(azul=(Fore.BLUE + " O "), rojo=(Fore.RED + " O "), amarillo=(Fore.YELLOW + " O "), verde=(Fore.GREEN + " O "), reset=Fore.RESET)
     Juego.elegirModo()
 
 if __name__ == "__main__":
-    # Inicializador del archivo.
     main()
