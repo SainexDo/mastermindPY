29e70a2a56403ea26dfdd9605f572b656655015f
SainexDo <ventizgene@gmail.com>
2024-08-05 13:22:49 -0600

    La computadora ya puede generar secuencias aleatorias.

--- app/game.py
+++ app/game.py
@@ -1,42 +1,47 @@
 from colorama import Fore
 from time import sleep
 import keyboard
-
+import random
 
 class Game:
     #Juego, dividido en sus respectivas clases y métodos.
-    def __init__(self):
+    def __init__(self, rojo, azul, verde, amarillo, reset):
         #Declaramos variables privadas.
+        self.rojo = rojo
+        self.azul = azul
+        self.verde = verde
+        self.amarillo = amarillo
+        self.reset = reset
         self.secuencia = []
-        self.intentoDeSecuencia = []
-
-
+        self.intentoDeAdivinarSecuencia = []
 
     def elegirModo(self):
         while True:
-            print(f'¿Adivinas o creas la secuencia? (Adivinar / Crear)')
-            respuesta = input().strip().lower()
+            respuesta = input(f'¿Adivinas o creas la secuencia? (Adivinar / Crear): ').strip().lower()
             if respuesta not in ('adivinar', 'crear'):
                 print("Agrega una respuesta correcta.")
             else:
-                if respuesta == 'adivinar':
-                    self.creaComputadora()
-                elif respuesta == 'crear':
-                    self.creaJugador()
                 break
 
     def creaJugador(self):
-        print(Fore.GREEN + "FRAFRA" + Fore.RESET)
+        print("ga")
 
     def creaComputadora(self):
-        print("Crea Jugador.")
+        while True:
+            posiblesOpciones = (self.rojo, self.azul, self.amarillo, self.verde)
+            self.secuencia.insert(1, random.choice(posiblesOpciones) + self.reset)
+            if len(self.secuencia) == 4:
+                print(''.join(self.secuencia) + self.reset)
+                break
 
+        intentos = []
+        # while True:
+        #     print("Manejo")
+        #     break
 
 def main():
-    Juego = Game()
+    Juego = Game(azul=(Fore.BLUE + " O "), rojo=(Fore.RED + " O "), amarillo=(Fore.YELLOW + " O "), verde=(Fore.GREEN + " O "), reset=Fore.RESET)
     Juego.elegirModo()
-
-
 
 if __name__ == "__main__":
     # Inicializador del archivo.
